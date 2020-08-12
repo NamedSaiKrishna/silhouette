ed21ca8c0bebfe0056c4c9ea5777deeaf63c3ad7
Sai Krishna <49117132+NamedSaiKrishna@users.noreply.github.com>
2020-08-12 16:19:08 +0530

    Updates

---
+++ precache-manifest.949be70bcfd0c5e13a1fefc449a23fc4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df294109151f2c1c076e128e4f51fd38",
+    "url": "/silhouette/index.html"
+  },
+  {
+    "revision": "2d2f65bc88fae69c69fd",
+    "url": "/silhouette/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "94ce085394b4a7dd2af6",
+    "url": "/silhouette/static/js/2.abd0eb31.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/silhouette/static/js/2.abd0eb31.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d2f65bc88fae69c69fd",
+    "url": "/silhouette/static/js/main.9eb373b4.chunk.js"
+  },
+  {
+    "revision": "16be2b879eeb18f6c86f",
+    "url": "/silhouette/static/js/runtime-main.63ee9eb0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/silhouette/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.cf80de6f058da4a004da5c859e5d2de8.js"
+  "/silhouette/precache-manifest.949be70bcfd0c5e13a1fefc449a23fc4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/silhouette/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
