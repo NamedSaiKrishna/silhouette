fe4396b328113d28426738a8ec1caef97539fc55
Sai Krishna <49117132+NamedSaiKrishna@users.noreply.github.com>
2020-08-26 15:29:27 +0530

    Updates

--- precache-manifest.b361c3020a389fb4051244f78caa53b6.js
+++ precache-manifest.b361c3020a389fb4051244f78caa53b6.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "e0ea1c7689178b614ac2b74b329a46be",
+    "revision": "76bb3c14fb388c668cb3e0ce37f34249",
     "url": "/silhouette/index.html"
   },
   {
